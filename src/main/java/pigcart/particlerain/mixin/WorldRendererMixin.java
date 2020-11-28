d983c68836cd93e60921e8504db1c2cf659d477c
Max <pigcart@outlook.com>
2020-11-28 13:46:19 +0000

    made mixin less bad

--- src/main/java/pigcart/particlerain/mixin/WorldRendererMixin.java
+++ src/main/java/pigcart/particlerain/mixin/WorldRendererMixin.java
@@ -3,15 +3,14 @@ package pigcart.particlerain.mixin;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-    /**
-     * @author pigcart
-     * @reason can't be bothered to figure out a more elegant solution. this works. bye renderWeather!
-     */
-    @Overwrite
-    private void renderWeather(LightmapTextureManager manager, float f, double d, double e, double g) {
+    @Inject(at = @At("HEAD"), method = "renderWeather", cancellable = true)
+    private void renderWeather(LightmapTextureManager manager, float f, double d, double e, double g, CallbackInfo ci) {
+        ci.cancel();
     }
 }
