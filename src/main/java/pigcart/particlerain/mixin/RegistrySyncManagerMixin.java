943fa5dd4fc601964da31228bbc3aa1cafe2520c
PIGC <PigCart@outlook.com>
2024-06-26 19:43:37 +0100

    disable sync entirely (fix #38 & LAN Crash)

--- src/main/java/pigcart/particlerain/mixin/RegistrySyncManagerMixin.java
+++ src/main/java/pigcart/particlerain/mixin/RegistrySyncManagerMixin.java
@@ -1,28 +1,23 @@
 package pigcart.particlerain.mixin;
 
 import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerConfigurationPacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import pigcart.particlerain.ParticleRainClient;
-
-import java.util.Objects;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // Allows for other fabricapi users to join LAN worlds if they do not have the mod but the host player does.
 @Mixin(RegistrySyncManager.class)
 public abstract class RegistrySyncManagerMixin {
 
-    @Redirect(method = "createAndPopulateRegistryMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;getKey(Ljava/lang/Object;)Lnet/minecraft/resources/ResourceLocation;"))
-    private static @Nullable <T> ResourceLocation skipParticles(Registry<T> instance, T t) {
-        final ResourceLocation id = instance.getKey(t);
-        if (Objects.equals(id.getNamespace(), ParticleRainClient.MOD_ID)) {
-            return null;
-        } else {
-            return id;
-        }
+    @Inject(method = "configureClient", at = @At("HEAD"), cancellable = true)
+    private static void configureClient(ServerConfigurationPacketListenerImpl handler, MinecraftServer server, CallbackInfo ci) {
+        ci.cancel();
+        // trying to exclude individual registry entries from the sync is causing issues
+        // so I'm just going to disable the whole thing and call it a day. I'm sure it'll be fine.
+        // There should be an api in fabric to handle all this!
     }
 
 }
